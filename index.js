7c53d2c5e0aa4e00a5e1582d1147eaca67a91a2e
njmbb8 <nmcgr001@gmail.com>
2021-06-14 21:02:55 -0400

    commit tree 735b5ea4bd67a001941548d0be70744d2d4a2717

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    let matches = drivers.filter(function(driver){
+        if(driver.toLowerCase() === name.toLowerCase()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    })
+
+    return matches;
+}
+
+function fuzzyMatch(drivers, name) {
+    let matches = drivers.filter(function(driver){
+        if(driver.toLowerCase()[0] === name.toLowerCase()[0]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    })
+    return matches;
+}
+
+function matchName(drivers, name){
+    let matches = drivers.filter(function(driver){
+        if(driver.name.toLowerCase() === name.toLowerCase()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    })
+
+    return matches;
+}
